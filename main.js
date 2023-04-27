ae6f520e2ce5b720f474e2bafbf6784f34172ab0
Eliot Bas <eliot.bas@orange.fr>
2023-04-27 09:40:29 +0200

    add lenis smooth scroll

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 import './styles/main.scss'
+import Lenis from '@studio-freight/lenis'
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
 
 // Toggable Menu
 
